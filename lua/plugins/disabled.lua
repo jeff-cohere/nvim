b53a5ca9a35afe98b2f0a33a643988756dfdd5ab
Jeffrey N. Johnson <jeff@cohere-llc.com>
2024-06-01 12:24:34 -0700

    Some much-needed cutbacks.

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,6 @@
+return {
+  { "echasnovski/mini.pairs", enabled = false }, -- exceedingly irritating auto-pairing!
+  { "hrsh7th/nvim-cmp",       enabled = false }, -- mal-configured auto-complretion
+  { "nvimdev/dashboard-nvim", enabled = false },
+  { "stevearc/conform.nvim",  enabled = false },
+}
