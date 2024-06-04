8ad4ee4ecfbac8577f8c783764f63cfbf69527fb
Jeffrey N. Johnson <jeff@cohere-llc.com>
2024-06-03 17:42:09 -0700

    No autoformatting!

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,7 +3,7 @@
 -- Add any additional options here
 
 vim.opt_local.relativenumber = false -- absolute line numbers only, please
-
+vim.g.autoformat = false             -- please STOP FUCKING WITH MY CODE
 if vim.opt.colorcolumn then
   vim.opt.colorcolumn = '80'
   vim.cmd('highlight ColorColumn ctermbg=9')
