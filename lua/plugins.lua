9d5ae075f333b0dfc1297d2a8a1567abfdd7f046
Jeffrey N. Johnson <jeff@cohere-llc.com>
2024-10-08 20:00:22 -0700

    Switching to packer from LazyLua. Still needs cleanup!

---
+++ lua/plugins.lua
@@ -0,0 +1,107 @@
+-- packer: a Lua package manager that's NOT LazyVim!
+
+-- install packer if it's not already familiar
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+
+  -- packer, manage thyself
+  use 'wbthomason/packer.nvim'
+
+  -- snazzy buffer line
+  use 'akinsho/bufferline.nvim'
+
+  -- improved default UI
+  use 'stevearc/dressing.nvim'
+
+  -- search-label-powered navigation
+  use 'folke/flash.nvim'
+
+  -- git integration within buffers
+  use 'lewis6991/gitsigns.nvim'
+
+  -- indent guides
+  use 'lukas-reineke/indent-blankline.nvim'
+
+  -- lua-rocket-powered status line
+  use 'nvim-lualine/lualine.nvim'
+
+  -- a package manager for language servers, linters, etc: super dweeb stuff
+  use 'williamboman/mason-lspconfig.nvim'
+  use 'williamboman/mason.nvim'
+
+  -- pre-fab config files for language servers
+  use 'neovim/nvim-lspconfig'
+
+  -- file manager in a buffer
+  use 'nvim-neo-tree/neo-tree.nvim'
+
+  -- UI component library
+  use 'MunifTanjim/nui.nvim'
+
+  -- crazy-ass find/replace with dark powers
+  --use 'nvim-pack/nvim-spectre'
+
+  -- syntax highlighting, kicked up a notch
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'nvim-treesitter/nvim-treesitter-textobjects'
+
+  -- "all the lua functions I don't want to write twice"
+  --use 'nvim-lua/plenary.nvim'
+
+  -- highlighter of TODO:, FIXME:, and other such comments
+  use 'folke/todo-comments.nvim'
+
+  -- cutesy little diagnostic doots
+  use 'folke/trouble.nvim'
+
+  -- improved auto-formatting of code comments
+  use 'folke/ts-comments.nvim'
+
+  -- clippy for key bindings
+  use 'folke/which-key.nvim'
+
+  -- "fast as FUCK" auto-completion
+  use 'ms-jpq/coq_nvim'
+
+  -- highlight trailing whitespace
+  --use 'bronson/vim-trailing-whitespace'
+
+  -- Which lines have changed since our last Git commit?
+  --use 'airblade/vim-gitgutter'
+
+  -------------------
+  -- color schemes --
+  -------------------
+
+  -- gruvbox is best girl
+  use 'ellisonleao/gruvbox.nvim'
+
+  -- tokyonight is alright
+  use 'folke/tokyonight.nvim'
+
+  -- oldies but goodies
+  use 'iCyMind/NeoSolarized'
+  use 'drewtempelmeyer/palenight.vim'
+  use 'rakr/vim-one'
+  use 'mhartington/oceanic-next'
+
+  ---------------
+  -- postamble --
+  ---------------
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
+
