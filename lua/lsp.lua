80db12cfaba842be6dad721080603b2abfe0c4a3
Jeffrey N. Johnson <jeff@cohere-llc.com>
2025-02-17 17:03:35 -0800

    Added LspLogClear command.

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -115,3 +115,8 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
   end,
 })
+
+vim.api.nvim_create_user_command("LspLogClear", function()
+	local lsplogpath = vim.fn.stdpath("state") .. "/lsp.log"
+	if io.close(io.open(lsplogpath, "w+b")) == false then vim.notify("Clearing LSP Log failed.", vim.log.levels.WARN) end
+end, { nargs = 0 })
