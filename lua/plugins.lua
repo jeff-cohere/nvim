bed1252c5a8d6470b5fed73addb4694dac587667
Jeffrey N. Johnson <jeff@cohere-llc.com>
2026-05-02 12:37:00 -0700

    nvim-treesitter update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -79,7 +79,7 @@ require("lazy").setup({
     {
       'nvim-treesitter/nvim-treesitter',
       config = function()
-        require('nvim-treesitter.configs').setup {
+        require('nvim-treesitter.config').setup {
           -- a list of parser names, or "all" (the listed parsers MUST always be installed)
           ensure_installed = { "c", "go", "lua", "vim", "vimdoc", "query",
                                "markdown", "markdown_inline" },
