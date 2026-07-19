05159c1c2e2f5aca744f4acde3b1eb8f6c4f10a4
Jeffrey N. Johnson <jeff@cohere-llc.com>
2026-07-19 14:43:48 -0700

    Trading up to lastest neo-tree.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -151,6 +151,7 @@ require("lazy").setup({
     -- file manager in a buffer
     {
       'nvim-neo-tree/neo-tree.nvim',
+      branch = 'v3.x',
       dependencies = {
         'nvim-lua/plenary.nvim',
         'nvim-tree/nvim-web-devicons',
